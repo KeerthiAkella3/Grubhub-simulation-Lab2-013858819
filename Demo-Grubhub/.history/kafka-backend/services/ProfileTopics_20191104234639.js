e319aab0707aba91606369d1c329e803671dfa39
KeerthiAkella3 <Keerthy@MacBook-Pro-2.local>
2019-11-05 04:53:32 -0800

    update profiles

---
+++ Demo-Grubhub/.history/kafka-backend/services/ProfileTopics_20191104234639.js
@@ -0,0 +1,164 @@
+var Users = require('../models/UserSchema');
+
+exports.profileService = function profileService(msg, callback) {
+    console.log("In profile Service path:", msg.path);
+    switch (msg.path) {
+        case "buyerProfile":
+            buyerProfile(msg, callback);
+            break;
+        case "restaurantProfile":
+            restaurantProfile(msg, callback);
+            break;
+        case "updateBuyerProfile":
+            updateBuyerProfile(msg,callback);
+            break;
+        case "uploadRestaurantPicture":
+            uploadPicture(msg, callback);
+            break;
+        case "removePicture":
+            getPicture(msg, callback);
+            break;
+        case "removePicture":
+            getPicture(msg, callback);
+            break;
+    }
+};
+
+
+function buyerProfile(msg, callback) {
+
+    console.log("In get buyerProfile profile. Msg: ", msg);
+
+    Users.findOne({ '_id': msg.body.id }, { 'courses': 0, 'password': 0 }, function (err, results) {
+        if (err) {
+            console.log(err);
+            callback(err, "Database Error");
+        } else {
+            if (results) {
+                console.log("results:", results);
+                callback(null, { status: 200, profile: results });
+            }
+            else {
+                console.log("No results found");
+                callback(null, { status: 205 });
+            }
+        }
+    });
+}
+
+function restaurantProfile(msg, callback) {
+
+    console.log("In get restaurantProfile profile. Msg: ", msg);
+
+    Users.findOne({ '_id': msg.body.id }, { 'courses': 0, 'password': 0 }, function (err, results) {
+        if (err) {
+            console.log(err);
+            callback(err, "Database Error");
+        } else {
+            if (results) {
+                console.log("results:", results);
+                callback(null, { status: 200, profile: results });
+            }
+            else {
+                console.log("No results found");
+                callback(null, { status: 205 });
+            }
+        }
+    });
+}
+
+
+
+function getProfileImg(msg, callback) {
+
+    console.log("In get user profile. Msg: ", msg);
+
+    Users.findOne({ '_id': msg.body.id, 'role': msg.body.role }, {'_id': 0, 'img':1}, function (err, results) {
+        if (err) {
+            console.log(err);
+            callback(err, "Database Error");
+        } else {
+            if (results) {
+                console.log("results:", results);
+                callback(null, { status: 200, img: results.img });
+            }
+            else {
+                console.log("No results found");
+                callback(null, { status: 205 });
+            }
+        }
+    });
+}
+
+function updateProfile(msg, callback) {
+
+    console.log("In update profile. Msg: ", msg);
+    Users.findOneAndUpdate({ '_id': msg.body.id, 'role': msg.body.role }, { $set:
+        {
+            "name": msg.body.name,
+            "img": msg.body.img,
+            "phoneNumber": parseInt(msg.body.phoneNumber),
+            "aboutMe":msg.body.aboutMe,
+            "company":msg.body.company,
+            "city":msg.body.city,
+            "country": msg.body.country,
+            "school": msg.body.school,
+            "hometown": msg.body.hometown,
+            "languages": msg.body.languages,
+            "gender": msg.body.gender,
+          }
+    }, function (err, results) {
+        if (err) {
+            console.log(err);
+            callback(err, "Database Error");
+        } else {
+            if (results) {
+                console.log("results:", results);
+                callback(null, {status: 200});
+            }
+            else {
+                console.log("No results found");
+                callback(null, { status: 205 });
+            }
+        }
+    });
+}
+
+function uploadProfilePic(msg, callback) {
+
+    console.log("In upload profile pic. Msg: ", msg);
+    Users.findOneAndUpdate({ '_id': msg.body.id, 'role': msg.body.role }, { $set: {"img": msg.filename } }, function (err, results) {
+        if (err) {
+            console.log(err);
+            callback(err, "Database Error");
+        } else {
+            if (results) {
+                console.log("results:", results);
+                callback(null, {status: 200});
+            }
+            else {
+                console.log("No results found");
+                callback(null, { status: 205 });
+            }
+        }
+    });
+}
+
+function removeProfilePic(msg, callback){
+    console.log("In remove profile pic. Msg: ", msg);
+    Users.findOneAndUpdate({ '_id': msg.body.id, 'role': msg.body.role }, { $set: {"img": "" } }, function (err, results) {
+        if (err) {
+            console.log(err);
+            callback(err, "Database Error");
+        } else {
+            if (results) {
+                console.log("results:", results);
+                callback(null, {status: 200});
+            }
+            else {
+                console.log("No results found");
+                callback(null, { status: 205 });
+            }
+        }
+    });
+}
---
+++ Demo-Grubhub/.history/kafka-backend/services/ProfileTopics_20191104235252.js
@@ -0,0 +1,12 @@
+var Users = require('../models/UserSchema');
+var Restaurant = require('../models/RestaurantSchema');
+
+
+exports.profileService = function profileService(msg, callback) {
+    console.log("In profile Service path:", msg.path);
+    switch (msg.path) {
+        
+    }
+};
+
+
